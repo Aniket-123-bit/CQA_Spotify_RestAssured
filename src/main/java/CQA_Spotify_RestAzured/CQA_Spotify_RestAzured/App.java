188967b2b17e4b812e52a0df6489092a93b0b28c
Aniket-123-bit <anisunny782@gmail.com>
2022-08-06 13:59:20 +0530

    first commit

---
+++ src/main/java/CQA_Spotify_RestAzured/CQA_Spotify_RestAzured/App.java
@@ -0,0 +1,13 @@
+package CQA_Spotify_RestAzured.CQA_Spotify_RestAzured;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
